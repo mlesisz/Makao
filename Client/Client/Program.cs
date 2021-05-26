d702d83ecd2494bd94b423ca7ee523ecc00cb496
mlesisz <mariusz.lesisz@gmail.com>
2021-05-26 10:28:47 +0200

    Initial commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Text.Encoding;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 500);
+            using (var socket = new Socket(SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    socket.Connect(endPoint);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine($"Couldn't connect to {endPoint.Address}:{endPoint.Port}");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine("Connected. Enter blank line to quit");
+                while (true)
+                {
+                    Console.Write("Enter your message: ");
+                    var message = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(message)) { break; }
+                    var data = ASCII.GetBytes(message);
+                    try
+                    {
+                        socket.Send(data);
+                    }
+                    catch (Exception e)
+                    {
+                        break;
+                    }
+                    var buffer = new byte[1024];
+                    int dataSize = socket.Receive(buffer);
+                    var responseMsg = ASCII.GetString(buffer
+                        .Take(dataSize)
+                        .ToArray());
+                    if (string.IsNullOrWhiteSpace(responseMsg)) { break; }
+                    Console.WriteLine($"Response: {responseMsg}");
+                }
+                Console.WriteLine("Disconnected from server");
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 500);
+            using (var socket = new Socket(SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Bind(endPoint);
+                socket.Listen(10);
+
+                while (true)
+                {
+                    using( var connection = socket.Accept())
+                    {
+                        while (true)
+                        {
+                            byte[] buffer = new byte[1024];
+                            int len = 0;
+                            try
+                            {
+                                len = connection.Receive(buffer);
+                            }
+                            catch (Exception e)
+                            {
+                                break;
+                            }
+                            if (len == 0) break;
+                            connection.Send(buffer.Take(len).ToArray());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
